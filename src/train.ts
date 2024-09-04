fe38db8a669e4ec03db5bc449ee4539d9e38cb28
Joseph <kamoldinovq@gmail.com>
2024-09-04 22:13:12 +0900

    feat: H-TASK added to train.ts file

--- src/train.ts
+++ src/train.ts
@@ -1,7 +1,15 @@
-// TASK-G
-function getHighestIndex(w: number[]): number {
-  const engkatta = w.slice().sort((a, b) => b - a);
-  return w.indexOf(engkatta[0]);
+// H-TASK
+function getPositive(a: number[]): string {
+  const positives = a.filter((num) => num > 0);
+  return positives.join("");
 }
 
-console.log(getHighestIndex([55, 2, 12, 21, 21, 80, 100]));
+console.log(getPositive([1, -4, 2, 1]));
+
+// // TASK-G
+// function getHighestIndex(w: number[]): number {
+//   const engkatta = w.slice().sort((a, b) => b - a);
+//   return w.indexOf(engkatta[0]);
+// }
+
+// console.log(getHighestIndex([55, 2, 12, 21, 21, 80, 100]));
