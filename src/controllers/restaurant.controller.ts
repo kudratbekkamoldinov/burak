f28a04dceb81d5be54a7af53095bafc37022e6b5
Joseph <kamoldinovq@gmail.com>
2024-09-10 16:28:19 +0900

    feat: create restaurant member.related router get and post  endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -9,6 +9,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     // LOGIC
     // SERVICE MODEL .....
     res.send("Home Page");
+    // send | json | redirect | end | render
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -31,5 +32,25 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     console.log("Error, getSignup:", err);
   }
 };
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+    try {
+      console.log('processLogin');
+      res.send("DONE");
+    } catch (err) {
+      console.log("Error, processLogin:", err);
+    }
+  };
+
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+    try {
+      console.log('processSignup');
+      res.send("DONE");
+    } catch (err) {
+      console.log("Error, processSignup:", err);
+    }
+  };
+
 
 export default restaurantController;
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -4,8 +4,16 @@ import restaurantController from "./controllers/restaurant.controller";
 
 routerAdmin.get("/", restaurantController.goHome);
 
-routerAdmin.get("/login", restaurantController.getLogin);
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login/process", restaurantController.processLogin);
 
-routerAdmin.get("/singup", restaurantController.getSignup);
+routerAdmin
+  .get("/singup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
+
+/**  Product */
+
+/**  User */
 
 export default routerAdmin;
