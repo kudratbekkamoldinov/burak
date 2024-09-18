1a3d7b768723454a72c3e827515be3e6e9b21432
Joseph <kamoldinovq@gmail.com>
2024-09-18 16:26:02 +0900

    feat: create sessions on login signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -54,7 +54,6 @@ class MemberService {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-    console.log("exist:", exist);
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
 
     const salt = await bcrypt.genSalt();
