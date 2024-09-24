5d8f1be010d6967ecefb378afbc96474cd4e95a3
Joseph <kamoldinovq@gmail.com>
2024-09-24 17:51:44 +0900

    feat: getAllProducts api and pass data into ejs

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -11,7 +11,10 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
-    res.render("products");
+    const data = await productService.getAllProducts();
+    console.log("data:", data);
+
+    res.render("products", { products: data });
   } catch (err) {
     console.log("Error, getAllProducts:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -42,7 +45,6 @@ productController.createNewProduct = async (
     );
 
     console.log("data", data);
-
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
